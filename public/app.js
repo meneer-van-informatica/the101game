b89f098289d79b88494d3901d69e4ee7f44aa665
meneer-van-informatica <dbn@fioretti.nl>
2025-09-10 22:58:50 +0200

    paf

---
+++ public/app.js
@@ -0,0 +1,4 @@
+function startGame() {
+    alert("Game starting...!"); // Hier kan je verder gaan met de game-logica
+    // Hier kan je de logica voor de game-start toevoegen (zoals route naar een andere pagina)
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+// Importeren van vereiste modules
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;  // Gebruik 3000 of een andere poort
+
+// Serve statische bestanden (bijvoorbeeld index.html)
+app.use(express.static(path.join(__dirname, 'public')));  // Zorg ervoor dat je de juiste map hebt
+
+// Default route die index.html serveert
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));  // Verwijst naar index.html
+});
+
+// Andere dynamische routes kunnen hier toegevoegd worden
+app.get('/about', (req, res) => {
+  res.send('<h1>About W0L1</h1><p>Welcome to the W0L1 Game. Get ready to play!</p>');
+});
+
+// Start de server
+app.listen(port, () => {
+  console.log(`Server draait op http://localhost:${port}`);
+});
