99b5b618272a8166c15ae1dbacbd4c65780fbdb5
Lucas de Bruin <lucas@school.nl>
2025-09-12 13:58:06 +0200

    feat(webapp): mobile-first shell + pwa manifest + sw; live set()/slot()

---
+++ httpdocs/sw.js
@@ -0,0 +1,11 @@
+const c = 'the101game-cache-v1';
+const assets = ['/', '/index.html', '/manifest.webmanifest'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(c).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==c && caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
